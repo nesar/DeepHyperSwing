ca83de6ca4092078e31da20143d1fe4c57002ab3
Nesar Ramachandra <nayaknesar@gmail.com>
2024-03-18 18:06:30 -0500

    Update load_modules.sh

--- saul/load_modules.sh
+++ saul/load_modules.sh
@@ -1,5 +1,5 @@
-# Note the CUDA version from cudatoolkit (11.4)
-module load cudnn/8.2.0
-module load PrgEnv-nvidia cudatoolkit python
-module load cudnn/8.2.0
-conda activate dh
+module load PrgEnv-nvidia cray-mpich craype-accel-nvidia80 python
+module load cudatoolkit/12.2
+module load cudnn/8.9.3_cuda12
+conda activate dh_env_2024
+export MPICH_GPU_SUPPORT_ENABLED=1 
